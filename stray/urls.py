59efb929691963574620abe8c460033cbfa98e94
zinzin68 <dnwls3958@naver.com>
2023-05-17 16:14:53 +0900

    add pic

--- stray/urls.py
+++ stray/urls.py
@@ -3,11 +3,19 @@ from django.urls import path
 #index 는 대문 post_list는 게시판
 from stray.views import index, post_list, posting
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 app_name='stray'
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('', index,name='index'),
+
     path('post_list/', post_list, name='post_list'),
+
     path('post_list/<int:pk>/',posting, name='posting'),
 ]
+
+#이미지 url 설정
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
