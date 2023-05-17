e08df6fd093948c02db591a0b060d4f9312e9f65
zinzin68 <dnwls3958@naver.com>
2023-05-17 13:22:50 +0900

    posting detail

--- stray/urls.py
+++ stray/urls.py
@@ -1,9 +1,13 @@
+from django.contrib import admin
 from django.urls import path
-from .views import *
+#index 는 대문 post_list는 게시판
+from stray.views import index, post_list, posting
 
 app_name='stray'
 
 urlpatterns = [
-    path('', index),
-    path('post_list', post_list),
+    path('admin/',admin.site.urls),
+    path('', index,name='index'),
+    path('post_list/', post_list, name='post_list'),
+    path('post_list/<int:pk>',posting, name='posting'),
 ]
--- stray/views.py
+++ stray/views.py
@@ -6,7 +6,11 @@ from .models import Post
 def index(request):
     return render(request,'stray/index.html')
 
+
 def post_list(request):
     postlist = Post.objects.all()
     return render(request,'stray/post_list.html',{'postlist':postlist})
 
+def posting(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'stray/posting.html',{'post':post})
